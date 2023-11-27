a87ce7e9506157d7471a7b500bc308be53ddaa00
HanyoSun <119835098+HanyoSun@users.noreply.github.com>
2023-11-27 14:24:29 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let hasmouseMoved = false;
+let img1,img2;
+
+
+function preload(){
+img1 = loadImage("./nos.png");
+img2 =loadImage("./col.png");
+
+}
+
+
+
+function setup(){
+createCanvas(windowWidth,windowWidth);
+
+imageMode(CENTERT)
+img1 = loadImage
+
+}
+
+
+
+
+
